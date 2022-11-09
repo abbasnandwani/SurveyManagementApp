b80fa04b600412bc5c1ff270321562edba313ce2
Abbas Nandwani <anandwani@microsoft.com>
2022-11-09 14:26:09 +0000

    Created DBSchema

---
+++ DBSchema/SurveyDBSchema.sql
@@ -0,0 +1,106 @@
+﻿USE [SurveyDb]
+GO
+/****** Object:  Table [dbo].[QuestionTypes]    Script Date: 09/11/2022 14:22:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[QuestionTypes](
+	[Id] [int] NOT NULL,
+	[TypeName] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_QuestionTypes] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[SectionQuestions]    Script Date: 09/11/2022 14:22:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SectionQuestions](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[SectionId] [int] NOT NULL,
+	[Question] [nvarchar](max) NOT NULL,
+	[QuestionTypeId] [int] NOT NULL,
+	[AnswerValues] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Questions] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Sections]    Script Date: 09/11/2022 14:22:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Sections](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[SurveyId] [int] NOT NULL,
+	[Name] [nvarchar](1000) NOT NULL,
+ CONSTRAINT [PK_Sections] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[SurveyResponseJsons]    Script Date: 09/11/2022 14:22:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SurveyResponseJsons](
+	[SurveyId] [int] NOT NULL,
+	[ResponseInJson] [nvarchar](max) NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[SurveyResponses]    Script Date: 09/11/2022 14:22:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SurveyResponses](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[SurveyId] [int] NOT NULL,
+	[SectionId] [int] NOT NULL,
+	[SectionQuestionId] [int] NOT NULL,
+	[Question] [nvarchar](max) NOT NULL,
+	[AnswerResponse] [nvarchar](max) NOT NULL,
+ CONSTRAINT [PK_SurveyResponses] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Surveys]    Script Date: 09/11/2022 14:22:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Surveys](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](1000) NOT NULL,
+	[Description] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Surveys] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[SectionQuestions]  WITH CHECK ADD  CONSTRAINT [FK_SectionQuestions_Sections] FOREIGN KEY([SectionId])
+REFERENCES [dbo].[Sections] ([Id])
+GO
+ALTER TABLE [dbo].[SectionQuestions] CHECK CONSTRAINT [FK_SectionQuestions_Sections]
+GO
+ALTER TABLE [dbo].[Sections]  WITH CHECK ADD  CONSTRAINT [FK_Sections_Surveys] FOREIGN KEY([SurveyId])
+REFERENCES [dbo].[Surveys] ([Id])
+GO
+ALTER TABLE [dbo].[Sections] CHECK CONSTRAINT [FK_Sections_Surveys]
+GO
+ALTER TABLE [dbo].[SurveyResponseJsons]  WITH CHECK ADD  CONSTRAINT [FK_SurveyResponseJsons_Surveys] FOREIGN KEY([SurveyId])
+REFERENCES [dbo].[Surveys] ([Id])
+GO
+ALTER TABLE [dbo].[SurveyResponseJsons] CHECK CONSTRAINT [FK_SurveyResponseJsons_Surveys]
+GO
